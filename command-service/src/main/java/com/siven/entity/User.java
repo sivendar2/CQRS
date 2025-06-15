aa79f5512336275949af7394d54781b2fb0d2b5d
sivendar <sivendarp2023@gmail.com>
2025-06-15 13:41:10 -0400

    second changes

--- command-service/src/main/java/com/siven/entity/User.java
+++ command-service/src/main/java/com/siven/entity/User.java
@@ -2,11 +2,15 @@ package com.siven.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
+import javax.annotation.processing.Generated;
 
 @Entity
 public class User {
     @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private String name;
     private String email;
